a2c23e5288331617742e4f0da2375de495d2aaec
povilas <p.vaitonis@gmail.com>
2017-10-05 21:20:48 +0300

    Changed  httpClientModel source and added results variable for strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  results: string[];
+    // Inject HttpClient into your component or service.
+    constructor(private http: HttpClient) {}
 }
